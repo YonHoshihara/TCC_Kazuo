799744c7c3b03439db628d771b223e2a730b0b19
YonHoshihara <hoshiharyon@gmail.com>
2019-08-29 19:21:29 -0400

    Destroy Enemy

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    public string tag;
+    public float destroyTime;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == tag)
+        {
+            yield return new WaitForSeconds(destroyTime);
+            Destroy(gameObject);
+        }
+
+        yield return new WaitForSeconds(0.0f);
+    }
+}
