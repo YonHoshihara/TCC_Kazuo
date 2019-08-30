41b464393f440666ba9a6388f02c3fe3765c9ce5
YonHoshihara <hoshiharyon@gmail.com>
2019-08-29 20:07:32 -0400

    Adding Monster moviment script

---
+++ Assets/Scripts/MonsterMoviment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMoviment : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 addForceVector;
+        addForceVector.x = 0;
+        addForceVector.y = 0;
+        addForceVector.z = speed;
+        gameObject.GetComponent<Rigidbody>().AddForce(addForceVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
