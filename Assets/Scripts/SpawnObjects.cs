8b8aeca523545b03f4cabe979519f265e5404bd8
YonHoshihara <hoshiharyon@gmail.com>
2019-08-29 20:31:35 -0400

    Starting Spqawnpoint script

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] spawnPoints;
+    public GameObject[] objectsToSpawn;
+    public  float timeRepeatSpawn;
+    public float delayToStartSpawm;
+    void Start()
+    {
+        InvokeRepeating("Spawn", delayToStartSpawm, timeRepeatSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Length - 1)], (spawnPoints[Random.Range(0, spawnPoints.Length - 1)]));
+    }
+}
